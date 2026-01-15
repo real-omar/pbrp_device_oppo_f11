b97718264c669928689127cacec06c7d6642ead0
Omar? Omar! <158614055+real-omar@users.noreply.github.com>
2026-01-15 22:00:32 +0200

    Update my_callback.sh

--- recovery/root/my_callback.sh
+++ recovery/root/my_callback.sh
@@ -2,7 +2,7 @@
 
 # Use a relative path from the root of your OrangeFox source
 # DO NOT put a "/" at the start if it's inside your project folder
-SOURCE_IMAGE="device/oppo/CPH1989/addon/maintainer.png"
+SOURCE_IMAGE="device/oppo/CPH1911/addon/maintainer.png"
 
 # Target location in the recovery ramdisk
 TARGET_DIR="$FOX_RAMDISK/twres/images/Default/About"
