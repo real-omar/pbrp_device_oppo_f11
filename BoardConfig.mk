d77b4456bc937acd5d7b2a342dae085b4e87a604
Omar? Omar! <158614055+real-omar@users.noreply.github.com>
2026-01-15 21:59:22 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/oppo/CPH1989
+DEVICE_PATH := device/oppo/CPH1911
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
